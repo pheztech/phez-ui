b30be52455e75813fe774bc04bfb1952f70ce283
Ken Pham <klpham1016@gmail.com>
2021-03-28 11:47:40 -0400

    DataGrid: explicitly set type

--- src/components/DataGrid.tsx
+++ src/components/DataGrid.tsx
@@ -31,7 +31,7 @@ const DataGrid: React.FC<Props> = ({ columns, rows, rowCount, ...props }) => {
 							<path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
 						</svg>
 					</button>
-					{Array.from(Array(pageCount).keys()).map((e) => <button key={e} onClick={() => setPage(e)} className={`p-2 rounded-md text-base ${e === page ? 'bg-gray-300' : 'hover:bg-gray-100 active:bg-gray-300 hover:text-gray-900'}`}><div className='block w-5 h-5'>{e + 1}</div></button>)}
+					{Array.from(Array(pageCount).keys()).map((e: number) => <button key={e} onClick={() => setPage(e)} className={`p-2 rounded-md text-base ${e === page ? 'bg-gray-300' : 'hover:bg-gray-100 active:bg-gray-300 hover:text-gray-900'}`}><div className='block w-5 h-5'>{e + 1}</div></button>)}
 					<button onClick={next} className='datagrid-page-button'>
 						<svg className='w-5 h-5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 							<path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
