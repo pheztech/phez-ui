3e31413ade2ea8496bd406485bd69f1476bf6410
Ken Pham <klpham1016@gmail.com>
2021-03-28 11:49:54 -0400

    add all form components to index

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,12 @@ export { default as Modal } from './components/Modal'
 export { default as Paper } from './components/Paper'
 export { default as Portal } from './components/Portal'
 export { default as Table } from './components/Table'
+
+export { default as Checkbox } from './components/form/Checkbox'
+export { default as NumberField } from './components/form/NumberField'
+export { default as PriceField } from './components/form/PriceField'
+export { default as Radio } from './components/form/Radio'
+export { default as SelectField } from './components/form/SelectField'
 export { default as TextArea } from './components/form/TextArea'
 export { default as TextField } from './components/form/TextField'
-
+export { default as TimeField } from './components/form/TimeField'
